d55a07a66fca9dc25f6de6ccdedc2158b32f65f4
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2020-03-11 12:32:24 +0100

    Rewrote niceScroll.js. Re-added refference.

--- javascript/niceScroll.js
+++ javascript/niceScroll.js
@@ -1,34 +1,65 @@
 // Scroll effects
-// Set vars
-var i = 0;
-var scrollY = 0;
-var userYScrollPosition = window.pageYOffset;
-var targetYScrollPosition = 0;
-var bodyHeight = 0;
-var yPosition = 0;
-var animator = false;
-var distance = 40;
 
-// Scrolling effect
-function AutoScrollEffect(destination){
-	i = i + 1;
-	console.log('Initiating scroll. Please stand by. ' + i);
-	userYScrollPosition = window.pageYOffset;
-	targetYScrollPosition = document.getElementById(destination).offsetTop;
-	bodyHeight = document.body.offsetHeight;
-	yPosition = userYScrollPosition + window.innerHeight;
-	doAnimation = setTimeout('AutoScrollEffect(\''+destination+'\')',24);
-	if(yPosition > bodyHeight){
-		console.log('Scolled to bottom!');
-		clearTimeout(doAnimation);
+/*
+Obkective: enable smooth scrolling¨with browser states.
+1. Get sitename
+2. get Id if pressent
+3. Remove Id if pressent 
+4. Get navigation sections.
+5. Merge into single array
+6. Find all a selectors in navigations
+7. Loop all links
+8. Create a unque case for each iteration
+9. Get id target from href
+10. Add onclick event to scoll to the id and push new adress name to adressbar
+11. Remove href (conflicts with code)
+*/
+
+// Set vars
+var siteName = window.location.href;
+var siteIdRefference = siteName.split("#");
+// Remove Id from siteName
+siteName = siteIdRefference[0];
+var headNav = document.getElementById("headNav");
+var sideNav = document.getElementById("sideNav");
+var cases = document.getElementById("cases");
+var headNavLinks = Array.prototype.slice.call(headNav.querySelectorAll("a"));
+var sideNavLinks = Array.prototype.slice.call(sideNav.querySelectorAll("a"));
+var casesLinks = Array.prototype.slice.call(cases.querySelectorAll("a"));
+var allNav = headNavLinks.concat(sideNavLinks);
+allNav = allNav.concat(casesLinks);
+
+// Remove href, add onclick for smooth scorlling to sections and update adressbar.
+for (i = 0; i < allNav.length; i++) {
+   (function(index) {
+		// Get id target from href
+		var id = allNav[index].getAttribute("href");
+		if(id.length>1){
+			id = id.slice(1);
+		}
+		// Add onclick event to scoll to the id
+		allNav[index].addEventListener("click", function(){goTo(id)});
+		// Remove href
+		allNav[index].removeAttribute("href");
+   })(i);
+}
+
+
+function goTo(id){
+	if(id == "#"){
+		// Scroll smoothly to top
+		window.scroll({
+		top: 0, 
+		left: 0, 
+		behavior: 'smooth'
+		// Update adress bar
+	});
+		window.history.pushState(siteName, document.title, "");
 	} else {
-		if(userYScrollPosition < targetYScrollPosition-distance){
-			console.log('Scolling!');
-		    scrollY = userYScrollPosition+distance;
-		    window.scroll(0, scrollY);
-	    } else {
-			console.log('Scrolled to target!');
-		    clearTimeout(doAnimation);
-	    }
+		var destination = document.getElementById(id);
+		// Scroll smoothly to section
+		destination.scrollIntoView({ behavior: 'smooth' });
+		// Update adress bar
+		window.history.pushState(siteName, document.title, "#"+id);
 	}
 }
